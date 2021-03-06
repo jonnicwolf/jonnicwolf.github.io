1bd98a293b20e92dd7054e0c3253ad0af03caba7
Jon Narine <jonnicwolf@gmail.com>
2021-03-06 00:25:38 -0500

    header names added

---
+++ js/index.js
@@ -0,0 +1,23 @@
+//document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('#header')
+    const govName = 'Jon Narine'
+    const nickNamee = 'Fezz'
+header.textContent = govName
+    console.log(this)
+    this.style  
+            
+    console.log('hello world')
+            
+    setInterval(() => {
+        if (header.textContent === govName) {
+            header.textContent = nickName
+            
+        } else { header.textContent = govName }
+    },2000)
+
+
+
+    const ul = document.getElementsById('#nav-list')
+    ul.style.textDecoration = 'none'
+//})
+

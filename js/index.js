cbd0e5378b0956d739cda941150891da12307291
Jon Narine <jonnicwolf@gmail.com>
2021-03-10 18:58:46 -0500

    add some framework

--- js/index.js
+++ js/index.js
@@ -1,23 +1,50 @@
-//document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector('#header')
-    const govName = 'Jon Narine'
-    const nickNamee = 'Fezz'
+const header = document.querySelector('#header')
+const govName = 'Jon Narine'
+const nickName = 'Fezz'
+const documentHeight = document.documentElement.scrollHeight - window.innerHeight
+const scrollHeight = window.scrollY
+
 header.textContent = govName
-    console.log(this)
-    this.style  
-            
-    console.log('hello world')
-            
-    setInterval(() => {
-        if (header.textContent === govName) {
-            header.textContent = nickName
-            
-        } else { header.textContent = govName }
-    },2000)
+console.log(`${header.textContent} header here`)
+header.setAttribute('style','font-family: Bungee Shade; font-size: 48px')  
+
+const nameSwitch = () => {
+    if (header.textContent === govName) {
+        header.textContent = nickName
+        //name should switch on click to the toggle button then content switch
+        //add a fade animation that goes from opacity: 0 --> 1 --> 0 in 4000
+    } else {
+        header.textContent = govName        
+    }    
+}
+
+//names should swap every 4000 but switch on toggle button click
+setInterval(nameSwitch, 4000)
+
+//
 
 
 
-    const ul = document.getElementsById('#nav-list')
-    ul.style.textDecoration = 'none'
-//})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

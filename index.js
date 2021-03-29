8942fa25316ededfbd1aea0b5c9addb99e1903a5
Jon Narine <jonnicwolf@gmail.com>
2021-03-29 11:45:34 -0400

    housekeeping

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const headshotObj = [
-  { shot: '/photo_assets/5A06D71D-808D-4825-B692-F5C79E6D89B8.jpg' },
-  { shot: '/photo_assets/IMG_1062.jpeg' },
-  { shot: '/photo_assets/IMG_1948.jpeg' },
-  { shot: '/photo_assets/IMG_0830.jpeg' },  
-]
-
 const headShot = () => {
   const frame = document.querySelector('#headshot-wrap');
   const photo = document.createElement('img');
@@ -12,41 +5,6 @@ const headShot = () => {
   frame.innerHTML = photo.innerHTML(headshotObj[1].shot)
   console.log(photo.innerHTML(headshotObj[1].shot))
 }
-
-// headshot()
-
-//trying to make a continuous loop here
-// const headshotLoop = () => {
-//   const headshot = document.querySelector('#headshot-wrap')
-//   const img = document.createElement('img')
-//   headshot.innerHTML = ''
-//   headshot.appendChild(img)
-//   for (let i = 0; i <= headshotObj.length; i++) {    
-//     // img.src = headshotObj[i].shot
-//     console.log(img)
-//     setTimeout(() => img.src = headshotObj[i].shot, 1500)    
-//   }
-// }
-//setInterval(headshotLoop,1500)
-
-const aboutMeObj = [
-    { slide1Content: 'My name is Jon, but there are a million Jons around so call me Fezz!' },
-    { slide2Content: 'I was born in a small South American country called Guyana to a Soldier dad and a Geologist mom. I love cricket, Formula 1, coding, IndyCar, mountain biking, coding, boxing, and programming =D !' },
-    { slide3Content: 'I am kind, conscientious, direct, and I love working with others! None of us is as smart as all of us.'}
-];
-
-const skillsObj = [
-    {language: 'Javascript', image: 'javascript png'},
-    {language: 'HTML5', image: 'HTML5 png'},
-    {language: 'CSS', image: 'CSS png'},
-    {language: 'node.js', image: 'node.js'},
-    {language: 'react', image: 'react png'},
-    {language: 'Command Line', image: 'commandline png'}
-];
-
-const projectObj = [
-    { gif:'dragonrend png', content: 'blah'},
-];
 
 const header = document.querySelector('#header')
 const govName = 'Jon Narine.dev'
@@ -74,8 +32,9 @@ setInterval(nameSwitch, 4000)
 
 //modified from https://www.w3schools.com/howto/howto_js_slideshow.asp
 let slideIndex = 1;
-const showSlides = (n) => {    
-  const slides = document.getElementsByClassName("mySlides");
+const showSlides = (n,slideId) => {    
+  const slides = document.getElementsByClassName(slideId);
+  console.log(slides)
   
   if (n > slides.length) {
     slideIndex = 1
@@ -87,11 +46,11 @@ const showSlides = (n) => {
   }  
   slides[slideIndex-1].style.display = "block";    
 }
-showSlides(slideIndex);
+showSlides(slideIndex, 'mySlides1');
+showSlides(slideIndex,'mySlides2')
 
 const slideToDa= (n) => { showSlides(slideIndex += n) }
 const currentSlide = (n) => { showSlides(slideIndex = n) }
-
 
 //make the semicircle officially a button
 const semiCircle = document.querySelector('#semi-circle-btn')
